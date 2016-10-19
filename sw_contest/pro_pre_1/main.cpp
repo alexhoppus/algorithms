3f281799b61dc526fc683fec1ab2ba18ebe747b1
Alexander Yashchenko <a.yashchenko@samsung.com>
2016-10-19 13:12:27 +0300

    pro_pre_1 first solution working for 4 sample testcases
    Signed-off-by: Alexander Yashchenko <a.yashchenko@samsung.com>

--- sw_contest/pro_pre_1/main.cpp
+++ sw_contest/pro_pre_1/main.cpp
@@ -1,85 +1,238 @@
 #include <iostream>
-#include <stdlib.h>
-#include <string.h>
 
 #define MAX_N 2000
-
+//#define DEBUG 1
 using namespace std;
+
+int adj_list[MAX_N][MAX_N];
+int adj_list_cost[MAX_N][MAX_N];
+int n_adj_list[MAX_N];
 struct edje {
-	short v1;
-	short v2;
-	short cost;
-	void set(short vtx1, short vtx2, short c)
-	{
-		v1 = vtx1;
-		v2 = vtx2;
-		cost = c;
-	}
+	int v1;
+	int v2;
+	int cost;
 };
 
-struct node {
-	short adj[MAX_N];
-	short adj_cost[MAX_N];
-	short n_adj;
+int edge_cost(struct edje *e)
+{
+	return e->cost;
+}
 
-	void add_neighbour(short v, short cost) {
-		adj_cost[n_adj] = cost;
-		adj[n_adj++] = v;
+int visited[MAX_N];
+
+int get_cost(int v1, int v2)
+{
+	for (int i = 0; i < n_adj_list[v1]; i++) {
+		if (adj_list[v1][i] == v2)
+			return adj_list_cost[v1][i];
+	}
+}
+
+static inline void set_visited(int v1)
+{
+	visited[v1] = 1;
+}
+
+static inline int is_visited(int v1)
+{
+	return visited[v1];
+}
+struct edje edjes[MAX_N];
+static int n_edjes = 0;
+
+struct edje forbidden_edje;
+
+void set_forbidden_edje(struct edje e)
+{
+	forbidden_edje.v1 = e.v1;
+	forbidden_edje.v2 = e.v2;
+}
+
+static inline bool is_edje_forbidden(int v1, int v2)
+{
+	if ((forbidden_edje.v1 == v1 && forbidden_edje.v2 == v2)
+		|| (forbidden_edje.v1 == v2 && forbidden_edje.v2 == v1))
+		return true;
+	return false;
+}
+
+void put_to_edje_arr(int v1, int v2, int cost)
+{
+	edjes[n_edjes].v1 = v1;
+	edjes[n_edjes].v2 = v2;
+	edjes[n_edjes].cost = cost;
+	n_edjes++;
+}
+
+void memset(void *ptr, int val, unsigned long nr)
+{
+	for (unsigned int i = 0; i < nr; i++)
+		*((int *) ptr + i) = val;
+}
+
+void _add_edje(int v1, int v2, int cost)
+{
+	adj_list[v1][n_adj_list[v1]] = v2;
+	adj_list_cost[v1][n_adj_list[v1]] = cost;
+	n_adj_list[v1]++;
+}
+
+void add_edje(int v1, int v2, int cost)
+{
+	_add_edje(v1, v2, cost);
+	_add_edje(v2, v1, cost);
+	put_to_edje_arr(v1, v2, cost);
+}
+
+#define N_MAX_QUEUE 1024*1024
+template <class T>
+class queue {
+	T *_store[N_MAX_QUEUE];
+	T **qbegin;
+	T **qend;
+public:
+	void push(T* e) {
+		*qend = e;
+		qend++;
 	}
 
-	bool is_leaf() {
-		return n_adj ? true : false;
+	T *pop() {
+		T *elem = *qbegin;
+		qbegin++;
+		return elem;
 	}
-};
 
-struct u_tree {
-	struct node nodes[MAX_N];
+	int size() {
+		return (int) (qend - qbegin);
+	}
+	int vsize() {
+		return (int) (qend - &_store[0]);
+	}
 
-	struct edje e[MAX_N];
-	short n_edjes;
-	struct edje forbidden_edje;
-
-	u_tree() {
-		memset(nodes, 0x0, MAX_N * sizeof(struct node));
+	void flush() {
+		qbegin = _store;
+		qend =_store;
+	}
+	queue() : qbegin(_store), qend(_store) {
 	};
-
-	void add_edje(short v1, short v2, short cost) {
-		nodes[v1].add_neighbour(v2, cost);
-		nodes[v2].add_neighbour(v1, cost);
-		e[n_edjes++].set(v1, v2, cost);
-	}
-
-	void set_forbidden_edje(struct edje e) {
-		forbidden_edje = e;
-	}
 };
+
+
+struct vertex_dist {
+	int v;
+	int d;
+};
+
+struct vertex_dist vd[MAX_N];
+static int n_vd = 0;
+
+queue<struct vertex_dist> q;
+
+struct vertex_dist *alloc_vd()
+{
+	if (n_vd + 1 > MAX_N)
+		cout << "OVERFLOW alloc_vd" << endl;
+	return &vd[n_vd++];
+}
+
+struct vertex_dist *bfs(struct vertex_dist *vd)
+{
+	int max_d = 0;
+	struct vertex_dist *max_vd = vd;
+	memset(visited, 0x0, MAX_N);
+
+	q.push(vd);
+	set_visited(vd->v);
+#if DEBUG
+	cout << "BFS started from vertex " << vd->v << endl;
+#endif
+
+	while(q.size()) {
+		struct vertex_dist *vd = q.pop();
+		if (vd->d > max_vd->d) {
+#if DEBUG
+			cout << "max_vd set to " << vd->d << endl;
+#endif
+			max_vd = vd;
+		}
+		int *v = &vd->v;
+#if DEBUG
+		cout << "qsize " << q.size() <<  " v " << vd->v << " dist " << vd->d << " n_adj_list[*v] " << n_adj_list[*v] << endl;
+#endif
+		for (int i = 0; i < n_adj_list[*v]; i++) {
+			int *neighbour = &adj_list[*v][i];
+#if DEBUG
+			cout << i << ": v " << *neighbour << endl;
+#endif
+			if (!is_edje_forbidden(*v, *neighbour) && !is_visited(*neighbour)) {
+				struct vertex_dist *neighbour_vd = alloc_vd();
+				neighbour_vd->v = *neighbour;
+				neighbour_vd->d = vd->d + get_cost(*neighbour, *v);
+				q.push(neighbour_vd);
+				set_visited(*neighbour);
+			}
+		}
+	}
+	return max_vd;
+}
+
+int solve_for_vertex(int v)
+{
+	struct vertex_dist *vd = alloc_vd();
+	vd->v = v;
+	vd->d = 0;
+	struct vertex_dist *new_vd = bfs(vd);
+	new_vd->d = 0;
+	struct vertex_dist *max_vd = bfs(new_vd);
+	return max_vd->d;
+}
 
 int solve_testcase()
 {
-	int max_points = 0;
-	return max_points;
-}
+	int max_dist = solve_for_vertex(1);
 
-u_tree t;
+	for (int i = 0; i < n_edjes; i++) {
+		int cur_max_dist = 0;
+		struct edje e = edjes[i];
+		set_forbidden_edje(e);
+#if DEBUG
+		cout << "removing edge " << e.v1 << " " << e.v2 << endl;
+#endif
+		int d1 = solve_for_vertex(e.v1);
+		int d2 = solve_for_vertex(e.v2);
+#if DEBUG
+		cout << "d1 " << d1 << " d2 " << d2 << endl;
+#endif
+		cur_max_dist = d1 + d2 + edge_cost(&e);
+		if (max_dist < cur_max_dist)
+			max_dist = cur_max_dist;
+	}
+	return max_dist;
+}
 
 int main()
 {
 	int T, N;
 	cin >> T;
 	for (int i_t = 0;  i_t < T; i_t++) {
-		int max_points;
+		int max_dist;
+		memset(adj_list, 0x0, MAX_N * MAX_N);
+		memset(adj_list_cost, 0x0, MAX_N * MAX_N);
+		memset(n_adj_list, 0x0, MAX_N);
+		memset(edjes, 0x0, MAX_N);
+		memset(visited, 0x0, MAX_N);
+		memset(vd, 0x0, MAX_N);
+		n_edjes = 0;
+		n_vd = 0;
 		cin >> N;
 		for (int i_n = 0; i_n < N - 1; i_n++) {
 			short v1, v2, cost;
 			cin >> cost >> v1 >> v2;
-			t.add_edje(v1, v2, cost);
+			add_edje(v1, v2, cost);
 		}
 
-		for (int i_n = 0; i_n < N - 1; i_n++) {
-			t.set_forbidden_edje(t.e[i_n]);
-		}
-		max_points = solve_testcase();
-		cout << "#" << i_t + 1 << " " << max_points << endl;
+		max_dist = solve_testcase();
+		cout << "#" << i_t + 1 << " " << max_dist << endl;
 	}
 	return 0;
 }
