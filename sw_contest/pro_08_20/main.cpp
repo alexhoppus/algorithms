89f20892865619394eaea2e3fcf0a4d29cea9ae3
Alex Hoppus <alexhoppus111@gmail.com>
2016-11-19 15:59:57 +0300

    pro_08_20: Lets try this implementation
    Signed-off-by: Alex Hoppus <alexhoppus111@gmail.com>

--- sw_contest/pro_08_20/main.cpp
+++ sw_contest/pro_08_20/main.cpp
@@ -18,7 +18,7 @@ int cpu_used[MAX_CPU] = {0};
 
 char beenthere[MAX_N][MAX_LIMIT][MAX_LIMIT][MAX_LIMIT][MAX_LIMIT][MAX_LIMIT] = {0};
 
-#define DEBUG 1
+//#define DEBUG 1
 
 void memset(void *ptr, char val, unsigned long nr)
 {
@@ -41,12 +41,12 @@ static inline int cpu_queue_get(int idx, unsigned long long *cpu_queue)
 	ptr += idx;
 	return (int) *ptr;
 }
+
 static inline void set_beenthere(int pack_idx, unsigned long long *cpu_queue)
 {
 	int q[MAX_CPU];
-	for (int i = 0; i < MAX_CPU; i++) {
+	for (int i = 0; i < MAX_CPU; i++)
 		q[i] = cpu_queue_get(i, cpu_queue);
-	}
 #if DEBUG
 	printf("%s %d %d %d %d %d %d\n", __func__, pack_idx, q[0], q[1], q[2], q[3], q[4]);
 #endif
@@ -56,23 +56,17 @@ static inline void set_beenthere(int pack_idx, unsigned long long *cpu_queue)
 static inline bool get_beenthere(int pack_idx, unsigned long long *cpu_queue)
 {
 	int q[MAX_CPU];
-	for (int i = 0; i < MAX_CPU; i++) {
+	for (int i = 0; i < MAX_CPU; i++)
 		q[i] = cpu_queue_get(i, cpu_queue);
-	}
 #if DEBUG
 	printf("%s %d %d %d %d %d %d\n", __func__, pack_idx, q[0], q[1], q[2], q[3], q[4]);
 #endif
-	if (beenthere[pack_idx][q[0]][q[1]][q[2]][q[3]][q[4]] == '1')
-		return true;
-	else
-		return false;
+	return beenthere[pack_idx][q[0]][q[1]][q[2]][q[3]][q[4]] == '1' ? true : false;
 }
+
 static inline int cpu_queue_change(int idx, unsigned long long *cpu_queue, int val)
 {
 	char *ptr = (char *)cpu_queue;
-#if DEBUG
-//	printf("cpu_queue_change idx %d val %d cur value %d\n", idx, val, cpu_queue_get(idx, cpu_queue));
-#endif
 	ptr += idx;
 	val = val < -10 ? -10 : val;
 	*ptr += (char) val;
@@ -80,9 +74,6 @@ static inline int cpu_queue_change(int idx, unsigned long long *cpu_queue, int v
 		return 1;
 	if ((int)*ptr < 0)
 		*ptr = (char )0x0;
-#if DEBUG
-//	printf("changed value %d\n", cpu_queue_get(idx, cpu_queue));
-#endif
 	return 0;
 }
 
@@ -95,41 +86,36 @@ static inline void get_workload(int pack_idx, unsigned long long *cpu_queue)
 		cpu_queue_change(i_cpu, cpu_queue, -diff);
 }
 
-int solve(int cpu_idx, int pack_idx, unsigned long long cpu_queue)
+void solve(int cpu_idx, int pack_idx, unsigned long long cpu_queue)
 {
 	int ret = 0;
 	cpu_used[cpu_idx] = 1;
 #if DEBUG
-	//printf("solve: cpu_idx %d pack_idx %d ", cpu_idx, pack_idx);
-#endif
-	if (get_beenthere(pack_idx, &cpu_queue))
-		return 0;
-	set_beenthere(pack_idx, &cpu_queue);
-	if (ans != -1)
-		return 1;
-	if (pack_idx >= N) {
-		ans = cpu_used_to_ans();
-/*#if DEBUG
-		printf("ans = %d\n", ans);
-#endif*/
-		return 1;
-	}
-	get_workload(pack_idx, &cpu_queue);
-
-	if (cpu_queue_change(cpu_idx, &cpu_queue, L[pack_idx]))
-		return 0;
-#if DEBUG
-	printf("solve: cpu_idx %d pack_idx %d ", cpu_idx, pack_idx);
+	printf("solve: cpu_idx %d pack_idx %d L %d ", cpu_idx, pack_idx, L[pack_idx]);
 	for (int i = 0; i < cpus; i++) {
 		int cpu_i_q = cpu_queue_get(i, &cpu_queue);
 		printf(" cpu[%d] = %d ", i, cpu_i_q);
 	}
 	printf("\n");
 #endif
+	if (ans != -1)
+		return;
+	if (pack_idx >= N) {
+		ans = cpu_used_to_ans();
+		return;
+	}
+	get_workload(pack_idx, &cpu_queue);
+
+	if (cpu_queue_change(cpu_idx, &cpu_queue, L[pack_idx]))
+		return;
+
+	if (get_beenthere(pack_idx, &cpu_queue))
+		return;
+	set_beenthere(pack_idx, &cpu_queue);
+
 	pack_idx++;
 	for (int i = 0; i < cpus; i++)
-		ret |= solve(i, pack_idx, cpu_queue);
-	return ret;
+		solve(i, pack_idx, cpu_queue);
 }
 
 int main()
@@ -149,6 +135,6 @@ int main()
 			cin >> L[i_n];
 		}
 		solve(0, 0, cpu_queue);
-		printf("# %d %d", i_t + 1, ans);
+		printf("# %d %d\n", i_t + 1, ans);
 	}
 }
