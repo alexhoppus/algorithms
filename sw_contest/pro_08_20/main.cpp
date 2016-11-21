2f1d32485526a4a2a133ba260b90ae0040e41fa3
Alexander Yashchenko <a.yashchenko@samsung.com>
2016-11-21 13:49:20 +0300

    pro_08_20: Working solution
    Signed-off-by: Alexander Yashchenko <a.yashchenko@samsung.com>

--- sw_contest/pro_08_20/main.cpp
+++ sw_contest/pro_08_20/main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 using namespace std;
-#define MAX_N 1000
+#define MAX_N 1001
 #define MAX_CPU 5
 #define MAX_LIMIT 10
 
@@ -12,27 +12,17 @@ int L[MAX_N];
 int N;
 
 unsigned long long cpu_queue;
-int cpus = 5;
+int cpus;
 int ans;
-int cpu_used[MAX_CPU] = {0};
 
 char beenthere[MAX_N][MAX_LIMIT][MAX_LIMIT][MAX_LIMIT][MAX_LIMIT][MAX_LIMIT] = {0};
 
 //#define DEBUG 1
 
-void memset(void *ptr, char val, unsigned long nr)
+void memset(void *ptr, unsigned long long val, unsigned long nr)
 {
 	for (unsigned int i = 0; i < nr; i++)
-		*((char *) ptr + i) = val;
-}
-
-static inline int cpu_used_to_ans(void)
-{
-	int ret = 0;
-	for (int i = 0; i < MAX_CPU; i++)
-		if (cpu_used[i])
-			ret++;
-	return ret;
+		*((unsigned long long *) ptr + i) = val;
 }
 
 static inline int cpu_queue_get(int idx, unsigned long long *cpu_queue)
@@ -86,10 +76,9 @@ static inline void get_workload(int pack_idx, unsigned long long *cpu_queue)
 		cpu_queue_change(i_cpu, cpu_queue, -diff);
 }
 
-void solve(int cpu_idx, int pack_idx, unsigned long long cpu_queue)
+int solve(int cpu_idx, int pack_idx, unsigned long long cpu_queue)
 {
 	int ret = 0;
-	cpu_used[cpu_idx] = 1;
 #if DEBUG
 	printf("solve: cpu_idx %d pack_idx %d L %d ", cpu_idx, pack_idx, L[pack_idx]);
 	for (int i = 0; i < cpus; i++) {
@@ -99,23 +88,24 @@ void solve(int cpu_idx, int pack_idx, unsigned long long cpu_queue)
 	printf("\n");
 #endif
 	if (ans != -1)
-		return;
+		return 1;
 	if (pack_idx >= N) {
-		ans = cpu_used_to_ans();
-		return;
+		ans = 1;
+		return 1;
 	}
 	get_workload(pack_idx, &cpu_queue);
 
 	if (cpu_queue_change(cpu_idx, &cpu_queue, L[pack_idx]))
-		return;
+		return 0;
 
 	if (get_beenthere(pack_idx, &cpu_queue))
-		return;
+		return 0;
 	set_beenthere(pack_idx, &cpu_queue);
 
 	pack_idx++;
 	for (int i = 0; i < cpus; i++)
-		solve(i, pack_idx, cpu_queue);
+		ret |= solve(i, pack_idx, cpu_queue);
+	return ret;
 }
 
 int main()
@@ -125,16 +115,20 @@ int main()
 	cin >> t;
 	for (int i_t = 0; i_t < t; i_t++) {
 		cin >> N;
-		cpu_queue = 0;
-		ans = -1;
-		memset(beenthere, '0', MAX_N * MAX_LIMIT * MAX_LIMIT * MAX_LIMIT * MAX_LIMIT * MAX_LIMIT);
-		for (int i = 0; i < MAX_CPU; i++)
-			cpu_used[i] = 0;
 		for (int i_n = 0; i_n < N; i_n++) {
 			cin >> T[i_n];
 			cin >> L[i_n];
 		}
-		solve(0, 0, cpu_queue);
-		printf("# %d %d\n", i_t + 1, ans);
+		for (int i_cpu = 1; i_cpu <= MAX_CPU; i_cpu++) {
+			cpu_queue = 0;
+			ans = -1;
+			cpus = i_cpu;
+			memset(beenthere, 0x0, MAX_N * MAX_LIMIT * MAX_LIMIT * MAX_LIMIT * MAX_LIMIT * MAX_LIMIT / sizeof(unsigned long long));
+			if (solve(0, 0, cpu_queue)) {
+				ans = i_cpu;
+				break;
+			}
+		}
+		printf("#%d %d\n", i_t + 1, ans);
 	}
 }
